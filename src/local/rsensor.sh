31c60653f16e4fd268275e476b05eb38618bc5b2
Shusei Matoike <smat1957@gmail.com>
2024-04-22 02:44:12 +0900

    updated

--- src/local/rsensor.sh
+++ src/local/rsensor.sh
@@ -1,4 +1,7 @@
-#!/usr/bin/bash
+#!/usr/bin/sh
 ### sudo apt install -y sshpass ###
 dir='/home/mat/Documents'
-sshpass -p passion40 ssh mat@192.168.3.27 ~/Documents/sensor.sh >> "${dir}"/w.txt
+ip='192.168.3.27'
+pass='mypassword'
+usr='myuserid'
+sshpass -p "${pass}" ssh "${usr}"@"${ip}" ~/Documents/sensor.sh >> "${dir}"/w.txt
