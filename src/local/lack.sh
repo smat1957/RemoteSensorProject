f227e54c66b9edf11b50037d4119ebd54cb41700
Shusei Matoike <smat1957@gmail.com>
2024-05-10 07:12:20 +0900

    debug

--- src/local/lack.sh
+++ src/local/lack.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-TODAY=`date +%Y-%m-%d`
-searchstring="${TODAY}.txt"
-PWRD='passion40'
+TODAY="date +%Y-%m-%d"
+searchstring=${TODAY}.txt
+PASS='mypassword'
 USR='mat'
 ADDR='192.168.3.27'
 DIR='/home/mat/Documents'
-FILE_NAME="${DIR}"/lackfiles.txt
+FILE_NAME=${DIR}/lackfiles.txt
 while read LINE
 do
   match=$(echo "${LINE}" | awk "/^$searchstring/")
-  if [ -n "$match" ]; then
-    cp "${DIR}"/w.txt "${DIR}"/ww.txt
-    sshpass -p "${PWRD}" ssh "${USR}"@"${ADDR}" cat "${DIR}"/w.txt >> "${DIR}"/ww.txt
-    sort -u "${DIR}"/ww.txt > ${DIR}/www.txt
+  if [ -n "${match}" ]; then
+    cp ${DIR}/w.txt ${DIR}/ww.txt
+    sshpass -p ${PASS} ssh ${USR}@${ADDR} cat ${DIR}/w.txt >> ${DIR}/ww.txt
+    sort -u ${DIR}/ww.txt > ${DIR}/w.txt
     rm ${DIR}/ww.txt
-    mv ${DIR}/www.txt ${DIR}/ww.txt
   else
-    RC=$(sshpass -p ${PWRD} ssh ${USR}@${ADDR} 'test -f ${DIR}/data/${LINE};echo $?')
+    CMD=test\ -e\ ${DIR}/data/${LINE}
+    RC=$(sshpass -p ${PASS} ssh ${USR}@${ADDR} ${CMD};echo $?)
     if [ ${RC} -eq 0 ]; then
-      sshpass -p "${PWRD}" ssh "${USR}"@"${ADDR}" cat "${DIR}"/data/"${LINE}" > "${DIR}"/dataalt/"${LINE}"
-    fi 
-  fi 
+      sshpass -p ${PASS} scp ${USR}@${ADDR}:${DIR}/data/${LINE} ${DIR}/dataalt/${LINE}
+    fi
+  fi
 done < ${FILE_NAME}
 
