b8102ae83eab54af7f68acddcd5ec6be2baf5978
Shusei Matoike <smat1957@gmail.com>
2024-05-13 11:02:35 +0900

    debug

--- src/local/lack.sh
+++ src/local/lack.sh
@@ -1,31 +1,28 @@
-#!/bin/bash
+#!/usr/bin/bash
 TODAY=$(date +%Y-%m-%d)
 PASS='mypassword'
 USR='mat'
 ADDR='192.168.3.27'
 DIR='/home/mat/Documents'
 cp ${DIR}/w.txt ${DIR}/bkup.txt
-source ${DIR}/venv11/bin/activate
-  ${DIR}/venv11/bin/python3 ${DIR}/lackdata.py | \
+python3 ${DIR}/lackdata.py | \
 while read LINE; do (
-  grep ^"${LINE}" ${DIR}/w.txt > ${DIR}/"${LINE}".wrk
-  sshpass -p ${PASS} ssh -n ${USR}@${ADDR}\
-    grep ^"${LINE}" ${DIR}/w.txt >> ${DIR}/"${LINE}".wrk
-  CMD=test\ -e\ ${DIR}/data/"${LINE}".txt
+  grep ^${LINE} ${DIR}/bkup.txt > ${DIR}/${LINE}.wrk
+  sshpass -p ${PASS} ssh -n ${USR}@${ADDR} \
+    "grep ^${LINE} ${DIR}/w.txt" >> ${DIR}/${LINE}.wrk
+  CMD=test\ -e\ ${DIR}/data/${LINE}.txt
   RC=$(sshpass -p ${PASS} ssh -n ${USR}@${ADDR} ${CMD};echo $?)
   if [ ${RC} -eq 0 ]; then
-    sshpass -p ${PASS} ssh -n ${USR}@${ADDR}\
-      grep ^"${LINE}" ${DIR}/data/${LINE}.txt >> ${DIR}/"${LINE}".wrk
+    sshpass -p ${PASS} ssh -n ${USR}@${ADDR} \
+      "grep ^${LINE} ${DIR}/data/${LINE}.txt" >> ${DIR}/${LINE}.wrk
   fi
-  sort ${DIR}/"${LINE}".wrk > ${DIR}/"${LINE}".wrk2
-  uniq ${DIR}/"${LINE}".wrk2 > ${DIR}/"${LINE}".wrk
-  rm ${DIR}/"${LINE}".wrk2
+  sort ${DIR}/${LINE}.wrk | uniq - > ${DIR}/${LINE}.wrk2
+  rm ${DIR}/${LINE}.wrk
   match=$(echo "${LINE}" | awk "/^${TODAY}/")
   if [ -n "${match}" ]; then
-    cp ${DIR}/"${LINE}".wrk ${DIR}/w.txt
+    cp ${DIR}/${LINE}.wrk2 ${DIR}/w.txt
   else
-    cp ${DIR}/"${LINE}".wrk ${DIR}/data/"${LINE}".txt
+    cp ${DIR}/${LINE}.wrk2 ${DIR}/data/${LINE}.txt
   fi
-  rm ${DIR}/"${LINE}".wrk
+  rm ${DIR}/${LINE}.wrk2
 ) < /dev/null; done
-deactivate
