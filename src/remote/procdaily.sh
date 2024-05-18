7cc8ada76fe0f6e276eac59f6c73d8f66c7724d8
Shusei Matoike <smat1957@gmail.com>
2024-05-18 09:11:23 +0900

    updated

--- src/remote/procdaily.sh
+++ src/remote/procdaily.sh
@@ -4,10 +4,10 @@ dates=$(date +%Y-%m-%d)
 hour=$(date +%H)
 minu=$(date +%M)
 if [ ${hour} -ge 23 ]; then
-  if [ ${minu} -gt 58 ]; then
+  if [ ${minu} -gt 57 ]; then
     cp ${dir}/w.txt ${dir}/bkup.txt
     mv ${dir}/w.txt ${dir}/data/${dates}.txt
     touch ${dir}/w.txt
-    python3 ${dir}/upload.py ${dates}
+    python3 ${dir}/upload0.py ${dates}
   fi
 fi
