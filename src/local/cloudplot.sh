6bb237128657ad57c52a9316d9575e4257e5bbd9
Shusei Matoike <smat1957@gmail.com>
2024-05-26 11:34:56 +0900

    update

--- src/local/cloudplot.sh
+++ src/local/cloudplot.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/sh
 dir='/home/mat/Documents'
 today=$(date +%Y-%m-%d)
-yesterday=$(date --date "${today} 1 days ago" +%Y-%m-%d)
+dates=$(date --date "${today} 1 days ago" +%Y-%m-%d)
 days='1'
-if [ $# -eq 0 ]; then
-  days='1'
-  dates=$yesterday
-elif [ $# -eq 1 ]; then
+if [ $# -gt 0 ]; then
   days=$1
-  dates=$yesterday
-elif [ $# -eq 2 ]; then
-  days=$1
-  if [ $(date -d "$2" +%s) -ge $(date -d "${today}" +%s) ]; then
-    dates=$yesterday
-  else
+  if [ $# -eq 2 ]; then
+  if [ $(date -d "$2" +%s) -lt $(date -d "${today}" +%s) ]; then
     dates=$2
+  fi
   fi
 fi
 if [ $# -le 2 ]; then
